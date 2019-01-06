6456b86b11639cad7dddd4abea0550e2af1784f3
L <1053801783@qq.com>
2019-01-06 21:18:57 +0800

    加入home路由

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
 import React, {
   Component
 } from 'react';
+import { BrowserRouter, Route } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import store from './store'
 import Header from './components/Header/index'
+import Home from './pages/Home'
 class App extends Component {
-
   render () {
     return (
       <Provider store={store}>
-        <div className="App" >
+        <>
           <Header />
-        </div>
+          <BrowserRouter>
+            <>
+              <Route path='/' exact component={Home}></Route>
+            </>
+          </BrowserRouter>
+        </>
       </Provider>
     );
   }
