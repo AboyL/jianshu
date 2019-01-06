16efa5832d5c083a79aa3f11aa570d89b2f54e81
L <1053801783@qq.com>
2019-01-06 14:13:01 +0800

    部分代码优化

--- src/components/Header/store/actionsCreators.js
+++ src/components/Header/store/actionsCreators.js
@@ -1,19 +1,18 @@
 import * as actionTypes from './actionTypes'
 import axios from 'axios'
 import { fromJS } from 'immutable'
+const hotSearchList = (data) => ({
+  type: actionTypes.SET_HOT_SEARCH_LIST,
+  data: fromJS(data)
+})
+
 export const headerSearch = (value) => {
   return {
     type: actionTypes.SET_HEADER_SEARCH,
     value
   }
 }
-const hotSearchList = (data) => (
-  {
-    type: actionTypes.SET_HOT_SEARCH_LIST,
-    data: fromJS(data)
-  }
-)
-export const getHotSearchList = (dispatch) => {
+export const getHotSearchList = () => {
   return (dispatch) => {
     axios.post('api/hot_search_list').then((res) => {
       dispatch(hotSearchList(res.data.data))
--- src/components/Header/store/reducer.js
+++ src/components/Header/store/reducer.js
@@ -2,14 +2,15 @@ import * as actionTypes from './actionTypes'
 import { fromJS } from 'immutable'
 const defaultState = fromJS({
   headerSearch: '',
-  hotSearchList:[]
+  hotSearchList: []
 })
 export default (state = defaultState, actions) => {
-  if (actions.type === actionTypes.SET_HEADER_SEARCH) {
-    return state.set('headerSearch', actions.value)
+  switch (actions.type) {
+    case actionTypes.SET_HEADER_SEARCH:
+      return state.set('headerSearch', actions.value);
+    case actionTypes.SET_HOT_SEARCH_LIST:
+      return state.set('hotSearchList', actions.data);
+    default:
+      return state
   }
-  if (actions.type === actionTypes.SET_HOT_SEARCH_LIST) {
-    return state.set('hotSearchList', actions.data)
-  }
-  return state
 }
